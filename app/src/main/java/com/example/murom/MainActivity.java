a7688cb59f46bcd0a27d8dd989f8ca16380de643
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2024-01-13 20:57:09 +0700

    test(failed): image picker

--- app/src/main/java/com/example/murom/MainActivity.java
+++ app/src/main/java/com/example/murom/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.murom;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pickMedia(View v) {
+        Log.d("-->", "Hello pickMedia");
+
+        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    if (uri != null) {
+                        Log.d("-->", "Selected URI: " + uri);
+                    } else {
+                        Log.d("-->", "No media selected");
+                    }
+                });
+
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
+                .build());
     }
 }
