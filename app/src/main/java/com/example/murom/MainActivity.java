c93660c5c6f98b4c87250e10b25b2d92bb6c4072
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-14 16:53:33 +0700

    refactor: detect weird behavior when separating fn

--- app/src/main/java/com/example/murom/MainActivity.java
+++ app/src/main/java/com/example/murom/MainActivity.java
@@ -34,8 +34,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // This runs well
         imageView = findViewById(R.id.avatar);
         Button uploadAvatarBtn = findViewById(R.id.upload_avatar);
         uploadAvatarBtn.setOnClickListener(view -> launcher.launch(new PickVisualMediaRequest.Builder().setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE).build()));
+
+        // This one crashes the app immediately without any erros
+        ImageView testImageView = findViewById(R.id.test_image);
+        Button testUploadImageBtn = findViewById(R.id.test_button);
+        testUploadImageBtn.setOnClickListener(view -> launchImagePicker(testImageView));
+    }
+
+    private void launchImagePicker(ImageView imageView) {
+        ActivityResultLauncher<PickVisualMediaRequest> launcher =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), o -> {
+                    if (o == null) {
+                        Toast.makeText(MainActivity.this, "No image selected!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Glide.with(getApplicationContext()).load(o).into(imageView);
+                    }
+                });
+
+        launcher.launch(new PickVisualMediaRequest.Builder().setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE).build());
     }
 }
