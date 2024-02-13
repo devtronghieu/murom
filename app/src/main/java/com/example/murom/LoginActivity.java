68867a478a06451e08100cbc7d62ab004f513b68
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-02-13 16:53:00 +0700

    refactor: implement google sign in successfully

--- app/src/main/java/com/example/murom/LoginActivity.java
+++ app/src/main/java/com/example/murom/LoginActivity.java
@@ -6,24 +6,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Objects;
 
@@ -49,23 +45,30 @@ public class LoginActivity extends AppCompatActivity {
 
         ActivityResultLauncher<IntentSenderRequest> activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartIntentSenderForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        try {
-                            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
-                            String idToken = credential.getGoogleIdToken();
-                            String username = credential.getId();
-                            String password = credential.getPassword();
-                            if (idToken !=  null) {
-                                String email = credential.getGoogleIdToken();
-                                Log.d("-->", "Got ID token: " + email);
-                            } else if (password != null) {
-                                Log.d("-->", "Got password.");
-                            }
-                        } catch (ApiException e) {
-                            Log.d("-->", "ApiException " + e.toString());
+                result -> {
+                    try {
+                        SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                        String idToken = credential.getGoogleIdToken();
+                        if (idToken !=  null) {
+                            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+
+                            mAuth.signInWithCredential(firebaseCredential).addOnCompleteListener(runnable -> {
+                                if (runnable.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user != null) {
+                                        Log.d("-->", "Welcome " + user.getEmail());
+                                    } else {
+                                        Log.d("-->", "Welcome null user!?");
+                                    }
+                                } else {
+                                    Log.d("-->", "Failed to signInWithCredential");
+                                }
+                            });
+                        } else {
+                            Log.d("-->", "User not found");
                         }
+                    } catch (ApiException e) {
+                        Log.d("-->", "ApiException " + e.toString());
                     }
                 }
         );
@@ -73,22 +76,13 @@ public class LoginActivity extends AppCompatActivity {
         Button googleLoginBtn = findViewById(R.id.google_login_btn);
         googleLoginBtn.setOnClickListener(v -> {
             oneTapClient.beginSignIn(signInRequest)
-                    .addOnSuccessListener(LoginActivity.this, new OnSuccessListener<BeginSignInResult>() {
-                        @Override
-                        public void onSuccess(BeginSignInResult result) {
-                            Log.d("-->", "onSuccess");
-                            IntentSenderRequest intentSenderRequest =
-                                    new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
-                            activityResultLauncher.launch(intentSenderRequest);
-                        }
+                    .addOnSuccessListener(LoginActivity.this, result -> {
+                        IntentSenderRequest intentSenderRequest = new IntentSenderRequest.Builder
+                                (result.getPendingIntent().getIntentSender()).build();
+                        activityResultLauncher.launch(intentSenderRequest);
                     })
-                    .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            // No saved credentials found. Launch the One Tap sign-up flow, or
-                            // do nothing and continue presenting the signed-out UI.
+                    .addOnFailureListener(LoginActivity.this, e -> {
                             Log.d("-->", "onFailure " + Objects.requireNonNull(e.getLocalizedMessage()));
-                        }
                     });
         });
 
@@ -101,7 +95,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            Log.d("-->", "User " + currentUser.getEmail());
+            Log.d("-->", "Welcome back, " + currentUser.getEmail());
         } else {
             Log.d("-->", "Not logged in");
         }
