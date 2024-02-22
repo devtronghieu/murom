6eb8a6cbc8328f2410da995305f4b45125bc9944
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-02-22 11:08:29 +0700

    ui: re-order bottom nav item + disable ripple effect

--- app/src/main/java/com/example/murom/MainActivity.java
+++ app/src/main/java/com/example/murom/MainActivity.java
@@ -13,23 +13,26 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    FloatingActionButton search;
+    FloatingActionButton home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        search = findViewById(R.id.bottom_nav_search);
+        home = findViewById(R.id.bottom_nav_home);
         setContentView(binding.getRoot());
         replaceFragment(new NewsfeedFragment());
+
+        binding.bottomNavigation.setItemActiveIndicatorEnabled(false);
+        binding.bottomNavigation.setItemRippleColor(null);
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
 
-            if (itemId == R.id.bottom_nav_newsfeed) {
-                replaceFragment(new NewsfeedFragment());
-            } else if (itemId == R.id.bottom_nav_post) {
+            if (itemId == R.id.bottom_nav_post) {
                 replaceFragment(new PostFragment());
+            } else if (itemId == R.id.bottom_nav_search) {
+                replaceFragment(new SearchFragment());
             } else if (itemId == R.id.bottom_nav_reels) {
                 replaceFragment(new ReelsFragment());
             } else if (itemId == R.id.bottom_nav_profile) {
@@ -39,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        binding.floatingButtonSearch.setOnClickListener(v -> {
-            replaceFragment(new SearchFragment());
+        binding.bottomNavHome.setOnClickListener(v -> {
+            replaceFragment(new NewsfeedFragment());
         });
     }
 
