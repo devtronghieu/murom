d93bdb3e00a2ccc83294b08a93dd4b7a7555845f
Le Thanh Trung <letrung148257@gmail.com>
2024-02-24 03:13:53 +0700

    update profile UI (not finished)

---
+++ app/src/main/java/com/example/murom/Recycler/ProflieAdapter.java
@@ -0,0 +1,9 @@
+package com.example.murom.Recycler;
+
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.murom.R;
+
+public class ProflieAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+    private  context context;
+
+}
