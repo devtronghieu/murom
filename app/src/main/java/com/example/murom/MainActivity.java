cdfd73a59360fc137506945b331e1233f8dc66b9
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-03-01 08:54:45 +0700

    refactor: bring stories data to main activity

--- app/src/main/java/com/example/murom/MainActivity.java
+++ app/src/main/java/com/example/murom/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.murom;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.murom.Firebase.Auth;
+import com.example.murom.Firebase.Database;
+import com.example.murom.Firebase.Schema;
 import com.example.murom.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton home;
@@ -29,23 +36,15 @@ public class MainActivity extends AppCompatActivity {
     Fragment profileFragment;
     Fragment storyFragment;
 
+    String uid;
+    HashMap<String, ArrayList<Schema.Story>> storiesMap = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        bottomMenu = binding.bottomNavigation;
-
-        fragmentContainer = findViewById(R.id.main_layout_fragment);
-        fullscreenFragmentContainer = findViewById(R.id.main_layout_fullscreen_fragment);
-
-        bottomMenu = findViewById(R.id.bottom_navigation);
-        bottomMenu.setItemActiveIndicatorEnabled(false);
-        bottomMenu.setItemRippleColor(null);
-
-        home = findViewById(R.id.bottom_nav_home);
+        // Setup env
+        uid = Auth.getUser().getUid();
 
         // Setup fragments
         fragmentManager = getSupportFragmentManager();
@@ -55,9 +54,22 @@ public class MainActivity extends AppCompatActivity {
         reelsFragment = new ReelsFragment();
         profileFragment = new ProfileFragment();
 
+        // Setup bottom nav
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomMenu = binding.bottomNavigation;
+
+        fragmentContainer = findViewById(R.id.main_layout_fragment);
+        fullscreenFragmentContainer = findViewById(R.id.main_layout_fullscreen_fragment);
+
+        home = findViewById(R.id.bottom_nav_home);
+
+        bottomMenu = findViewById(R.id.bottom_navigation);
+        bottomMenu.setItemActiveIndicatorEnabled(false);
+        bottomMenu.setItemRippleColor(null);
         bottomMenu.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
-
             if (itemId == R.id.bottom_nav_post) {
                 replaceFragment(postFragment);
             } else if (itemId == R.id.bottom_nav_search) {
@@ -69,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.bottom_nav_profile) {
                 replaceFragment(profileFragment);
             }
-
             return true;
         });
 
@@ -81,6 +92,19 @@ public class MainActivity extends AppCompatActivity {
         // Set default fragment to Newsfeed (Home icon)
         replaceFragment(newsfeedFragment);
         setHomeActive();
+
+        // Stories
+        Database.getStoriesByUID(this.uid, new Database.GetStoriesByUIDCallback() {
+            @Override
+            public void onGetStoriesSuccess(ArrayList<Schema.Story> stories) {
+                storiesMap.put(uid, stories);
+            }
+
+            @Override
+            public void onGetStoriesFailure() {
+                Toast.makeText(MainActivity.this, "Failed to load stories", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -124,7 +148,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleViewStory(String uid) {
-        storyFragment = new StoryFragment(uid, () -> removeFullscreenFragment(storyFragment));
-        addFullscreenFragment(storyFragment);
+        ArrayList<Schema.Story> stories = storiesMap.get(uid);
+        if (stories != null) {
+            storyFragment = new StoryFragment(stories, () -> removeFullscreenFragment(storyFragment));
+            addFullscreenFragment(storyFragment);
+        } else {
+            Toast.makeText(MainActivity.this, "Failed to view story of " + uid, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/murom/StoryFragment.java
+++ app/src/main/java/com/example/murom/StoryFragment.java
@@ -1,21 +1,18 @@
 package com.example.murom;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
-import com.example.murom.Firebase.Database;
 import com.example.murom.Firebase.Schema;
 
 import java.util.ArrayList;
@@ -32,13 +29,13 @@ public class StoryFragment extends Fragment {
     VideoView videoView;
     ConstraintLayout touchSurface;
 
-    String uid;
+
     StoryFragmentCallback callback;
     ArrayList<Schema.Story> stories;
     int currentStoryIndex = 0;
 
-    public StoryFragment(String uid, StoryFragmentCallback callback) {
-        this.uid = uid;
+    public StoryFragment(ArrayList<Schema.Story> stories, StoryFragmentCallback callback) {
+        this.stories = stories;
         this.callback = callback;
     }
 
@@ -50,20 +47,6 @@ public class StoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_story, container, false);
-
-        Database.getStoriesByUID(this.uid, new Database.GetStoriesByUIDCallback() {
-            @Override
-            public void onGetStoriesSuccess(ArrayList<Schema.Story> storyList) {
-                stories = storyList;
-                setStory(stories.get(currentStoryIndex));
-            }
-
-            @Override
-            public void onGetStoriesFailure() {
-                Toast.makeText(requireContext(), "Failed to load stories", Toast.LENGTH_SHORT).show();
-                progressBar.setVisibility(View.GONE);
-            }
-        });
 
         ImageButton closeBtn = rootView.findViewById(R.id.story_fragment_close_button);
         closeBtn.setOnClickListener(v -> callback.onClose());
@@ -80,9 +63,10 @@ public class StoryFragment extends Fragment {
                 currentStoryIndex = 0;
             }
 
-            Log.d("-->", "story: " + currentStoryIndex);
             setStory(stories.get(currentStoryIndex));
         });
+
+        setStory(stories.get(currentStoryIndex));
 
         return rootView;
     }
