614368e4aa5d18b17486eefbfd3d264f915d0c16
ltminhthu <ltminhthu.209@gmail.com>
2024-04-11 20:48:43 +0700

    adjust highlight bubble component

--- app/src/main/java/com/example/murom/Recycler/HighlightBubbleAdapter.java
+++ app/src/main/java/com/example/murom/Recycler/HighlightBubbleAdapter.java
@@ -3,6 +3,7 @@ package com.example.murom.Recycler;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,44 +20,46 @@ import java.util.ArrayList;
 public class HighlightBubbleAdapter extends RecyclerView.Adapter<HighlightBubbleAdapter.ViewHolder> {
     private Context context;
     private final ArrayList<HighlightBubbleModel> localDataSet;
-    //private final HighlightBubbleCallback callback;
+    private final HighlightBubbleCallback callback;
 
     public interface HighlightBubbleCallback {
-        void handleUploadHighlight();
-
-        void handleViewHighlight(String uid);
+        void handleEditHighlight(String highlightId);
+        void handleDeleteHighlight(String highlightId);
+        void handleViewHighlight(String highlightId);
+        void handleAddHighlight();
     }
 
     public static class HighlightBubbleModel {
-        private final String uid;
+        private final String highlightId;
         private final String imageUrl;
         private final String text;
 
-
-        public HighlightBubbleModel(String uid, String imageUrl, String text) {
-            this.uid = uid;
+        public HighlightBubbleModel(String highlightId, String imageUrl, String text) {
+            this.highlightId = highlightId;
             this.imageUrl = imageUrl;
             this.text = text;
         }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
-        private final LinearLayout highlightImageContainer;
         private final ImageView highlightImage;
-        private final ImageView uploadButton;
         private final TextView highlightText;
+        private final Button editHighlight;
+        private final Button deleteHighlight;
+        private final LinearLayout buttonsContainer;
         public ViewHolder(View view){
             super(view);
-            highlightImageContainer = view.findViewById(R.id.highlight_bubble_image_container);
             highlightImage = view.findViewById(R.id.highlight_bubble_image);
-            uploadButton =view.findViewById(R.id.highlight_bubble_upload);
             highlightText = view.findViewById(R.id.highlight_bubble_text);
-
+            editHighlight = view.findViewById(R.id.edit_highlight_btn);
+            deleteHighlight = view.findViewById(R.id.delete_highlight_btn);
+            buttonsContainer = view.findViewById(R.id.buttons_container);
         }
     }
 
-    public HighlightBubbleAdapter(ArrayList<HighlightBubbleModel> dataSet) {
+    public HighlightBubbleAdapter(ArrayList<HighlightBubbleModel> dataSet, HighlightBubbleCallback callback) {
         localDataSet = dataSet;
+        this.callback = callback;
     }
 
     @NonNull
@@ -73,12 +76,19 @@ public class HighlightBubbleAdapter extends RecyclerView.Adapter<HighlightBubble
         HighlightBubbleModel data = localDataSet.get(position);
         viewHolder.highlightText.setText(data.text);
         Glide.with(this.context).load(data.imageUrl).into(viewHolder.highlightImage);
-        //viewHolder.highlightImage.setOnClickListener(v -> callback.handleViewHighlight());
+        viewHolder.editHighlight.setOnClickListener(v -> callback.handleEditHighlight(data.highlightId));
+        viewHolder.deleteHighlight.setOnClickListener(v -> callback.handleDeleteHighlight(data.highlightId));
 
-        //if(position == 0){
-            //viewHolder.uploadButton.setVisibility(View.VISIBLE);
-            //viewHolder.uploadButton.setOnClickListener(v -> callback.handleUploadHighlight());
-        //}
+        viewHolder.highlightImage.setOnClickListener(v -> callback.handleViewHighlight(data.highlightId));
+        viewHolder.highlightImage.setOnLongClickListener(v -> {
+            viewHolder.buttonsContainer.setVisibility(View.VISIBLE);
+            return true;
+        });
+
+        if(position == 0){
+            viewHolder.highlightImage.setOnClickListener(v -> callback.handleAddHighlight());
+            viewHolder.highlightImage.setOnLongClickListener(v -> true);
+        }
     }
 
     @Override
